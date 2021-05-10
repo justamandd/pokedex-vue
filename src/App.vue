14cb360c1357cbde6176c495c19d8df8b7bc5d66
Justamandd <justamandd@gmail.com>
2021-05-10 01:49:20 -0300

    Success

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/pokedex.png" />
+    <br />
+    <div class="column is-half is-offset-one-quarter">
+      <input
+        class="input is-rounded"
+        type="text"
+        placeholder="Search"
+        v-model="busca"
+      />
+      <button
+        class="button is-fullwidth is-success"
+        id="buscaBtn"
+        @click="buscar"
+      >
+        Go
+      </button>
+      <div
+        v-for="(poke, index) in filteredPokemons"
+        :key="poke.url"
+        class="column"
+      >
+        <Pokemon :num="index" :name="poke.name" :url="poke.url" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Pokemon from "./components/Pokemon.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: "",
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        console.log("Pokemons resgatados!");
+        this.pokemons = res.data.results;
+        this.filteredPokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    Pokemon,
+  },
+  methods: {
+    buscar: function () {
+      this.filteredPokemons = this.pokemons;
+      if (this.busca == "" || this.busca == " ") {
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(
+          (pokemon) => pokemon.name == this.busca.toLowerCase()
+        );
+      }
+    },
+  },
+  computed: {
+    /*
+    resultBusc: function () {
+      if (this.busca == "" || this.busca == " ") {
+        return this.pokemons;
+      } else {
+        return this.pokemons.filter((pokemon) => pokemon.name == this.busca);
+      }
+    },*/
+  },
+};
 </script>
 
 <style>
@@ -24,5 +84,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscaBtn {
+  margin-top: 2%;
 }
 </style>
